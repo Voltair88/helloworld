275d6089a871fe5fb2dc13dcb6d6bd3487f20900
Mattias Gulec <Voltair88@gmail.com>
2022-10-10 23:18:32 +0200

    test

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        String x = "Hello, World!";
+        String x = "Hello, World2!";
         System.out.println(x);
     }
 }
