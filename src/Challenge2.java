a618350644c02ab7013bd0147cfcdaccaab57a65
Mattias Gulec <Voltair88@gmail.com>
2022-10-13 11:47:33 +0200

    codeacadamy1

--- src/Challenge2.java
+++ src/Challenge2.java
@@ -1,7 +1,7 @@
 public class Challenge2 {
     public static void main(String[] args) {
         String Mattias = "Mattias";
-        Number Age = 34;
+        int Age = 34;
         Boolean Married = true;
         Float Height = 1.80f;
         System.out.println("Hello, my name is " + Mattias + " and I am " + Age + " years old. It is " + Married
