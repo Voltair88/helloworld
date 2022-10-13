bcc284fdf69215122ab98980b5520cba59111a98
Mattias Gulec <Voltair88@gmail.com>
2022-10-13 10:26:13 +0200

    Challange 1 and 2 complete

---
+++ src/Challenge1.java
@@ -0,0 +1,8 @@
+public class Challenge1 {
+    public static void main(String[] args) {
+        Number Array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println(Array[i]);
+        }
+    }
+}
---
+++ src/Challenge2.java
@@ -0,0 +1,11 @@
+public class Challenge2 {
+    public static void main(String[] args) {
+        String Mattias = "Mattias";
+        Number Age = 34;
+        Boolean Married = true;
+        Float Height = 1.80f;
+        System.out.println("Hello, my name is " + Mattias + " and I am " + Age + " years old. It is " + Married
+                + " that i am married"
+                + " and my height is " + Height + " meters.");
+    }
+}
